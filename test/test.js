211005848eae659dc8c6c68463e23c292e367352
Rich-Harris <richard.a.harris@gmail.com>
2015-12-29 13:22:52 -0500

    be more permissive about helpers, including allowing runtime helpers (#17)

--- test/test.js
+++ test/test.js
@@ -32,12 +32,10 @@ describe( 'rollup-plugin-babel', function () {
 	this.timeout( 15000 );
 
 	it( 'runs code through babel', function () {
-		var start = Date.now();
 		return rollup.rollup({
 			entry: 'samples/basic/main.js',
 			plugins: [ babelPlugin() ]
 		}).then( function ( bundle ) {
-			start = Date.now();
 			var generated = bundle.generate();
 
 			var code = generated.code;
@@ -46,7 +44,7 @@ describe( 'rollup-plugin-babel', function () {
 		});
 	});
 
-	it( 'adds helpers', function () {
+	it.only( 'adds helpers', function () {
 		return rollup.rollup({
 			entry: 'samples/class/main.js',
 			plugins: [ babelPlugin() ]
@@ -54,7 +52,7 @@ describe( 'rollup-plugin-babel', function () {
 			var generated = bundle.generate();
 			var code = generated.code;
 
-			assert.ok( code.indexOf( 'babelHelpers.classCallCheck =' ) !== -1, generated.code );
+			assert.ok( code.indexOf( 'function babelHelpers_classCallCheck' ) !== -1, generated.code );
 			assert.ok( code.indexOf( 'var _createClass =' ) === -1, generated.code );
 		});
 	});
@@ -117,5 +115,18 @@ describe( 'rollup-plugin-babel', function () {
 			.catch( function ( err ) {
 				assert.ok( /es2015-rollup/.test( err.message ), 'Expected an error about external helpers or module transform, got "' + err.message + '"' );
 			});
+	});
+
+	it( 'allows transform-runtime to be used instead of bundled helpers', function () {
+		return rollup.rollup({
+			entry: 'samples/runtime-helpers/main.js',
+			plugins: [ babelPlugin({ runtimeHelpers: true }) ],
+			onwarn: function ( msg ) {
+				assert.equal( msg, `Treating 'babel-runtime/helpers/classCallCheck' as external dependency` );
+			}
+		}).then( function ( bundle ) {
+			var cjs = bundle.generate({ format: 'cjs' }).code;
+			assert.ok( !~cjs.indexOf( 'babelHelpers' ) );
+		});
 	});
 });
