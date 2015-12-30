f5f3787faef3d2cdaa966df7bbeb5c5d055d8cc7
Rich-Harris <richard.a.harris@gmail.com>
2015-12-29 22:17:24 -0500

    so confuse

--- src/index.js
+++ src/index.js
@@ -12,10 +12,7 @@ function preflightCheck ( localOpts ) {
 
 	if ( ~check.indexOf( 'import _classCallCheck from "babel-runtime' ) ) return RUNTIME;
 	if ( ~check.indexOf( 'function _classCallCheck' ) ) return INLINE;
-	if ( ~check.indexOf( 'babelHelpers.classCallCheck' ) ) return BUNDLED;
-
-	console.log( check );
-	console.log( check.indexOf( 'babelHelpers' ) );
+	if ( ~check.indexOf( 'babelHelpers' ) ) return BUNDLED;
 
 	throw new Error( 'An unexpected situation arose. Please raise an issue at https://github.com/rollup/rollup-plugin-babel/issues. Thanks!' );
 }
