c3c373654a75b682f878711e4f527e884a3615e2
Sebastian Werner <s.werner@sebastian-software.de>
2018-06-29 09:33:29 +0200

    Extracted error messages into variables, splitted by lines for better code readability. Template literals are not allowed right now by linting.

--- src/preflightCheck.js
+++ src/preflightCheck.js
@@ -2,6 +2,19 @@ import { join } from 'path';
 import { transform } from '@babel/core';
 import { INLINE, RUNTIME, EXTERNAL } from './constants.js';
 
+const MODULE_ERROR =
+	'Rollup requires that your Babel configuration keeps ES6 module syntax intact. ' +
+	'Unfortunately it looks like your configuration specifies a module transformer ' +
+	'to replace ES6 modules with another module format. To continue you have to disable it.' +
+	'\n\n' +
+	'Most commonly it\'s a CommonJS transform added by @babel/preset-env - ' +
+	'in such case you should disable it by adding `modules: false` option to that preset ' +
+	'(described in more detail here - https://github.com/rollup/rollup-plugin-babel#modules ).';
+
+const UNEXPECTED_ERROR =
+	'An unexpected situation arose. Please raise an issue at ' +
+	'https://github.com/rollup/rollup-plugin-babel/issues. Thanks!';
+
 function fallbackClassTransform () {
 	return {
 		visitor: {
@@ -38,14 +51,14 @@ export default function createPreflightCheck () {
 				!~check.indexOf( 'export default Foo' ) &&
 				!~check.indexOf( 'export { Foo as default }' )
 			) {
-				ctx.error( 'Rollup requires that your Babel configuration keeps ES6 module syntax intact. Unfortunately it looks like your configuration specifies a module transformer to replace ES6 modules with CommonJS. To continue you have to disable it. Most commonly it\'s a CommonJS transform added by @babel/preset-env - in such case you should disable it by adding `modules: false` option to that preset (described in more detail here - https://github.com/rollup/rollup-plugin-babel#modules ).' );
+				ctx.error( MODULE_ERROR );
 			}
 
 			if ( ~check.indexOf( '/helpers/inherits' ) ) helpers = RUNTIME;
 			else if ( ~check.indexOf( 'function _inherits' ) ) helpers = INLINE;
 			else if ( ~check.indexOf( 'babelHelpers' ) ) helpers = EXTERNAL;
 			else {
-				ctx.error( 'An unexpected situation arose. Please raise an issue at https://github.com/rollup/rollup-plugin-babel/issues. Thanks!' );
+				ctx.error( UNEXPECTED_ERROR );
 			}
 
 			preflightCheckResults[ dir ] = helpers;
