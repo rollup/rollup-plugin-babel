bec8df99faf37662a996c1ca2342484c6d26ffce
Bogdan Chadkin <trysound@yandex.ru>
2018-08-05 11:54:54 +0300

    Tweak files and revert external

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
 import buble from 'rollup-plugin-buble';
 import pkg from './package.json';
 
+const externalDeps = Object.keys(
+	Object.assign({}, pkg.dependencies, pkg.peerDependencies)
+);
+const nodeDeps = ['path'];
+const external = externalDeps.concat(nodeDeps);
+
 export default {
 	input: './src/index.js',
 	plugins: [ buble({ objectAssign: 'Object.assign' }) ],
-	external: id => !id.startsWith('.') && !id.startsWith('/'),
+	external,
 	output: [
 		{ file: pkg.main, format: 'cjs', sourcemap: true },
 		{ file: pkg.module, format: 'esm', sourcemap: true },
